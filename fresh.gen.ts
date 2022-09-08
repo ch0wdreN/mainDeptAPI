bdfa18a31dcef4ac4109757d7e7cae845f85c2d9
i19yanagi <i19yanagi@gmail.com>
2022-09-09 00:19:43 +0900

    update: auto generated

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,19 +2,19 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/index.tsx";
-import * as $$0 from "./islands/Counter.tsx";
+import * as $0 from './routes/api/rank.ts';
+import * as $1 from './routes/api/send.ts';
+import * as $2 from './routes/index.tsx';
+import * as $$0 from './islands/Counter.tsx';
 
 const manifest = {
   routes: {
-    "./routes/[name].tsx": $0,
-    "./routes/api/joke.ts": $1,
-    "./routes/index.tsx": $2,
+    './routes/api/rank.ts': $0,
+    './routes/api/send.ts': $1,
+    './routes/index.tsx': $2,
   },
   islands: {
-    "./islands/Counter.tsx": $$0,
+    './islands/Counter.tsx': $$0,
   },
   baseUrl: import.meta.url,
 };
