8241198ec3c392ac1b90f983fe3d0ed53a0d1511
i19yanagi <i19yanagi@gmail.com>
2022-09-16 01:40:52 +0900

    change: setup oak

--- main.ts
+++ main.ts
@@ -1,26 +1,7 @@
-/// <reference no-default-lib="true" />
-/// <reference lib="dom" />
-/// <reference lib="dom.asynciterable" />
-/// <reference lib="deno.ns" />
-/// <reference lib="deno.unstable" />
+import { Application } from 'oak/mod.ts';
+import router from './routes/route.ts';
 
-import { InnerRenderFunction, RenderContext, start } from '$fresh/server.ts';
-import manifest from './fresh.gen.ts';
+const app = new Application();
 
-import { config, setup } from '@twind';
-import { virtualSheet } from 'twind/sheets';
-
-const sheet = virtualSheet();
-sheet.reset();
-setup({ ...config, sheet });
-
-function render(ctx: RenderContext, render: InnerRenderFunction) {
-  const snapshot = ctx.state.get('twind') as unknown[] | null;
-  sheet.reset(snapshot || undefined);
-  render();
-  ctx.styles.splice(0, ctx.styles.length, ...(sheet).target);
-  const newSnapshot = sheet.reset();
-  ctx.state.set('twind', newSnapshot);
-}
-
-await start(manifest, { render });
+app.use(router.routes());
+await app.listen({ port: 8000 });
