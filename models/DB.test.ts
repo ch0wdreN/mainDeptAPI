87a217ec6f0cd2dee4659132e1f9838667e589d2
i19yanagi <i19yanagi@gmail.com>
2022-09-28 10:08:57 +0900

    change: fmt

--- models/DB.test.ts
+++ models/DB.test.ts
@@ -1,12 +1,12 @@
 import { assertEquals } from 'testing/asserts.ts';
 import { DB } from '@db';
 
-const db = new DB;
-await db.initialize()
+const db = new DB();
+await db.initialize();
 
 Deno.test('Database', async (t) => {
   await t.step('get result by name', async () => {
-    const result = await db.getResultByName("test");
-    assertEquals(result, [{name: "test", score: 100}]);
+    const result = await db.getResultByName('test');
+    assertEquals(result, [{ name: 'test', score: 100 }]);
   });
 });
