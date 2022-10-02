6f40984cdae6e1403fb5cc27417e7097db93b3f4
i19yanagi <i19yanagi@gmail.com>
2022-10-03 00:51:46 +0900

    change: change main.ts and change vr scripts

--- main.ts
+++ main.ts
@@ -4,6 +4,6 @@ import { oakCors } from 'cors';
 
 const app = new Application();
 
-app.use(oakCors())
-  .use(router.routes());
+app.use(oakCors());
+app.use(router.routes());
 await app.listen({ port: 8000 });
