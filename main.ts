7e64a558d17558813205aba48968b025c893b3fa
i19yanagi <i19yanagi@gmail.com>
2022-11-02 21:58:18 +0900

    :+1: enable cors for frontend

--- main.ts
+++ main.ts
@@ -4,6 +4,8 @@ import { oakCors } from 'cors';
 
 const app = new Application();
 
-app.use(oakCors());
+app.use(oakCors({
+  origin: ["https://maindept.vercel.app", "http://localhost:3000"]
+}));
 app.use(router.routes());
 await app.listen({ port: 8000 });
