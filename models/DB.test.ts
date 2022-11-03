62163e843727299a2a97bc89546274956e7ab96f
i19yanagi <i19yanagi@gmail.com>
2022-11-03 22:39:52 +0900

    :green_heart: update test

--- models/DB.test.ts
+++ models/DB.test.ts
@@ -6,15 +6,15 @@ const db = new DB();
 await db.initialize();
 
 Deno.test('Database', async (t) => {
-  await t.step('get result by name', async () => {
-    const result = await db.getResultByName('test');
-    assertEquals(result, [{ name: 'test', score: 100 }]);
-  });
   await t.step('post result', async () => {
-    const data: Result = { name: 'testUser', score: 200 };
+    const data: Result = { name: 'testUser', score: 100 };
     await db.postResult(data);
     const result = await db.getResultByName('testUser');
     assertEquals(result, [data]);
+  });  
+  await t.step('get result by name', async () => {
+    const result = await db.getResultByName('testUser');
+    assertEquals(result, [{ name: 'testUser', score: 100 }]);
   });
   await t.step('delete result', async () => {
     await db.deleteResult('testUser');
