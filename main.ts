4e5bda00cd2964f59eb7959547f4eb5adf071018
i19yanagi <i19yanagi@gmail.com>
2022-11-05 07:40:20 +0900

    :+1: add cors list

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ import { oakCors } from 'cors';
 const app = new Application();
 
 app.use(oakCors({
-  origin: ["https://maindept.vercel.app", "http://localhost:3000"]
+  origin: ["https://maindept.vercel.app", "http://localhost:3000", "http://localhost:4173"]
 }));
 app.use(router.routes());
 await app.listen({ port: 8000 });
